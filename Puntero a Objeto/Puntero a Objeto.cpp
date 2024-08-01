bf57922038f9486dd1fd5db9c1a7b6e8a5ddb39b
Brryan12 <bryancr1208@gmail.com>
2024-08-01 16:28:56 -0600

    j

--- Puntero a Objeto/Puntero a Objeto.cpp
+++ Puntero a Objeto/Puntero a Objeto.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
-#include <sstream>
+#include <sstream>  
 #include "Libro.h"
 //class inline
+class Libro {
+private:
+    std::string ISBN;
+    std::string titulo;
+public:
+    Libro(std::string ISBN, std::string titulo) {
+        std::cout << "Se creo" << std::endl;
+        this->ISBN = ISBN;
+        this->titulo = titulo;
+    }
+    virtual ~Libro() {
+        std::cout << "Se fue" << std::endl;
+    }
+    std::string getISBN() { return ISBN; }
+    std::string getTitulo() { return titulo; }
+    std::string toString() {
+        std::stringstream s;
+        s << "Libro" << std::endl << "ISBN: " << ISBN << std::endl << "Titulo: " << titulo << std::endl;
+        return s.str();
+
+    }
+
+};
 
 int main()
 {
