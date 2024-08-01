a3d209a854a578527c94fabafb3eff79a5ce28cc
Brryan12 <bryancr1208@gmail.com>
2024-08-01 16:05:29 -0600

    d

--- Puntero a Objeto/Puntero a Objeto.cpp
+++ Puntero a Objeto/Puntero a Objeto.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <sstream>
+//class inline
+
+class Libro {
+private:
+    std::string ISBN;
+    std::string titulo;
+public:
+    Libro(std::string ISBN, std::string titulo) {
+        this->ISBN = ISBN;
+        this->titulo = titulo;
+    }
+    virtual ~Libro() {}
+    std::string getISBN() { return ISBN; }
+    std::string getTitulo() { return titulo; }
+    std::string toString() {
+        std::stringstream s;
+        s << "Libro" << std::endl << "ISBN: " << ISBN << std::endl << "Titulo: " << titulo << std::endl;
+        return s.str();
+
+    }
+
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
+
+
+    return 0;
 }
 
 
