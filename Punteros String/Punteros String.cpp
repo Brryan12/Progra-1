1b2de45ddb99f977276a75d386424bc057cfe6eb
Brryan12 <bryancr1208@gmail.com>
2024-08-01 15:55:56 -0600

    h

--- Punteros String/Punteros String.cpp
+++ Punteros String/Punteros String.cpp
@@ -3,12 +3,16 @@ using namespace std;
 
 int main()
 {
-//Puntero a string
+	//Puntero a string
 	string nombre = "puntarenas";
-	cout << nombre<<endl;
+	cout << nombre << endl;
 
 	string nombre2 = string("Heredia");
-	cout << nombre2<<endl;
+	cout << nombre2 << endl;
+
+	string* ptrS = NULL;
+	ptrS = &nombre;
+	cout << *ptrS;
 
 	return 0;
 }
